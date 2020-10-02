27cff89c7a7ed1d871534533c0f5fe0e214622fa
vladimirm85 <vladimirm85@gmail.com>
2020-10-02 19:01:14 +0300

    feat: decode implemented

--- src/index.js
+++ src/index.js
@@ -37,9 +37,40 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
+const LETTER_LENGTH = 10;
+
+const DASH_DOT_LENGTH = 2;
+
+const DASH_DOT = {
+    10: ".",
+    11: "-",
+};
+
+const getCharsArray = (str) => {
+    const res = [];
+    for (let i = 0; i < str.length; i += LETTER_LENGTH) {
+        res.push(str.substr(i, LETTER_LENGTH));
+    }
+    return res;
+};
+
+const getMorseCode = (str) => {
+    let morseCode = "";
+    for (let i = 0; i < str.length; i += DASH_DOT_LENGTH) {
+        morseCode += DASH_DOT[str.substr(i, DASH_DOT_LENGTH)];
+    }
+    return morseCode;
+};
+
+const getChar = (str) => {
+    const firstOne = str.indexOf("1");
+    if (!~firstOne) return " ";
+    const morseCode = getMorseCode(str.slice(firstOne));
+    return MORSE_TABLE[morseCode];
+};
+
+const decode = (expr) =>
+  getCharsArray(expr).reduce((acc, el) => (acc += getChar(el)), "");
 
 module.exports = {
     decode
